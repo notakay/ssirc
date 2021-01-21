db83a6cf218e9fe6c4224201f97e355632e6f8bf
akay <aungkhantko@protonmail.com>
2021-01-21 17:19:18 +0630

    Created watcher thread

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::str::{from_utf8};
+use std::sync::{mpsc};
+use std::thread;
 
 use crate::threadpool::{ThreadPool};
 
@@ -8,18 +10,29 @@ pub fn run_server(port: usize) {
     let addr = format!("127.0.0.1:{}", port);
     let listener = TcpListener::bind(addr).unwrap();
     let pool = ThreadPool::new(2);
+    let (sender, receiver) = mpsc::channel();
+
+    thread::spawn( || watcher(receiver));
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        pool.execute ( || handle_connection(stream) );
+        let sender = mpsc::Sender::clone(&sender);
+        pool.execute ( || handle_connection(stream, sender) );
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn watcher(receiver: mpsc::Receiver<String>) {
+    for received in receiver {
+        print!("> {}", received);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, sender: mpsc::Sender<String>) {
     let mut buf = [0; 128];
     loop {
         let size = stream.read(&mut buf).unwrap();
-        print!("{}", from_utf8(&buf[0..size]).unwrap());
+        let message = from_utf8(&buf[0..size]).unwrap().to_string();
+        sender.send(message).unwrap();
         stream.write(&buf[0..size]).unwrap();
         buf = [0; 128];
     }
